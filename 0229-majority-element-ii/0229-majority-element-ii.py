9bea1e6e362e5ca475a4801776a38eddab8b2e9b
Archish77 <archishking@gmail.com>
2026-02-11 23:23:50 +0530

    Time: 3 ms (92.76%), Space: 22.1 MB (57.39%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def majorityElement(self, nums):
+        candidate1 = candidate2 = None
+        count1 = count2 = 0
+
+       
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+       
+        result = []
+        n = len(nums)
+
+        if candidate1 is not None and nums.count(candidate1) > n // 3:
+            result.append(candidate1)
+
+        if candidate2 is not None and candidate2 != candidate1 and nums.count(candidate2) > n // 3:
+            result.append(candidate2)
+
+        return result
