7e627eccd06209918740a02339f73281fb94dc3f
Archish77 <archishking@gmail.com>
2026-02-27 10:31:43 +0530

    Time: 22 ms (95.74%), Space: 24.7 MB (44.59%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):                                                         ##Queue Approach
+        self.q = deque()
+
+    def ping(self, t):
+
+        self.q.append(t)
+
+        # Remove calls older than t - 3000                           
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+
+        return len(self.q)
