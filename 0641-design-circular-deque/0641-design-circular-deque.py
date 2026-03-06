dc40cd8aec013984ecce882bb310b1cddfec401f
Archish77 <archishking@gmail.com>
2026-03-06 09:52:25 +0530

    Time: 3 ms (90.46%), Space: 19.8 MB (97.61%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -0,0 +1,64 @@
+class MyCircularDeque:
+
+    def __init__(self, k):
+        self.q = [0] * k
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+        self.capacity = k
+
+    def insertFront(self, value):
+
+        if self.isFull():
+            return False
+
+        self.front = (self.front - 1) % self.capacity
+        self.q[self.front] = value
+        self.size += 1
+        return True
+
+    def insertLast(self, value):
+
+        if self.isFull():
+            return False
+
+        self.rear = (self.rear + 1) % self.capacity
+        self.q[self.rear] = value
+        self.size += 1
+        return True
+
+    def deleteFront(self):
+
+        if self.isEmpty():
+            return False
+
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def deleteLast(self):
+
+        if self.isEmpty():
+            return False
+
+        self.rear = (self.rear - 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def getFront(self):
+
+        if self.isEmpty():
+            return -1
+        return self.q[self.front]
+
+    def getRear(self):
+
+        if self.isEmpty():
+            return -1
+        return self.q[self.rear]
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def isFull(self):
+        return self.size == self.capacity
